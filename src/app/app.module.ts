c50b082aaeb7e6a8ad4e5bb16a86997bdcf99861
Owner <owner@Owners-MacBook-Pro.local>
2018-12-09 14:07:23 -0600

    sentry addition

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, ErrorHandler, Injectable } from '@angular/core';
 
 import { AppComponent } from './app.component';
+
+import * as Sentry from '@sentry/browser';
+
+Sentry.init({
+  dsn: 'https://4fdd1e30e37746dfb347f92d23bcd9c7@sentry.io/1340312'
+});
+
+@Injectable()
+export class SentryErrorHandler implements ErrorHandler {
+  constructor() {}
+  handleError(error) {
+    Sentry.captureException(error.originalError || error);
+    throw error;
+  }
+}
 
 @NgModule({
   declarations: [
@@ -10,7 +25,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [{ provide: ErrorHandler, useClass: SentryErrorHandler }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
